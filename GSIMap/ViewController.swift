ab2153a380eb82b9908510ad9544816a1080fbe4
Tomohiro Tashiro <tashiro@wni.com>
2020-07-26 16:10:02 +0900

    Initial Commit

---
+++ GSIMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSIMap
+//
+//  Created by tasshy on 2020/07/26.
+//  Copyright © 2020 tasshy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
